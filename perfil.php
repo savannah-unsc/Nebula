316b38411b8e430807a189ea9e7941871aa539dc
savannah-unsc <ryanfeliciano.silva@outlook.com>
2022-09-20 07:59:57 -0300

    update

--- perfil.php
+++ perfil.php
@@ -131,7 +131,140 @@ if (isset($usuario) == false) {
         <div id='seguido'> <?php echo "<h1> Seguidores </h1> <h2 class='gray'> $seguidores </h2>"; ?> </div>
       </div>
     </div>
-    <div id="myposts" class="corpo"></div>
+    <div id="myposts" class="corpo">
+      <?php
+      
+      $sql = "SELECT * FROM posts where user_id = $idconvidado order by id desc";
+
+$result=mysqli_query($conn,$sql);
+while($tabela=mysqli_fetch_array($result))
+{
+$postid = $tabela['id'];
+$publisherid = $tabela['user_id'];
+$tipo = $tabela['tipo'];
+$midia = $tabela['midia'];
+$curtidas = $tabela['curtidas'];
+$msg = base64_decode($tabela['conteudo']);
+$comuserid = $tabela['com_user_id'];
+
+if ($curtidas >= 1000) {
+  $curtidas = $curtidas / 1000;
+  $curtidas = number_format($curtidas, 1, '.', '');
+  $curtidas = "$curtidas<b>k</b>";
+}
+
+if ($curtidas >= 1000000) {
+  $curtidas = $curtidas / 1000;
+  $curtidas = number_format($curtidas, 1, '.', '');
+  $curtidas = "$curtidas<b>M</b>";
+}
+
+if ($comuserid == 0 or $comuserid == $publisherid) {
+  $sqla = "SELECT * FROM users where id = $publisherid";
+$resultado=mysqli_query($conn,$sqla);
+if ($tabela=mysqli_fetch_array($resultado)) {
+  $publishername = $tabela['usuario'];
+  $publisheruid = $tabela['uid'];
+  $publishericon = $tabela['icon'];
+  $publisherbanner = $tabela['banner'];
+}
+} else {
+  $sqla = "SELECT * FROM users where id = $comuserid";
+  $resultado=mysqli_query($conn,$sqla);
+  if ($tabela=mysqli_fetch_array($resultado)) {
+  $publishername = $tabela['usuario'];
+  $publisheruid = $tabela['uid'];
+  $publishericon = $tabela['icon'];
+  $publisherbanner = $tabela['banner'];
+}
+$sqla = "SELECT * FROM users where id = $publisherid";
+$resultado=mysqli_query($conn,$sqla);
+if ($tabela=mysqli_fetch_array($resultado)) {
+  $coname = $tabela['usuario'];
+  $couid = $tabela['uid'];
+}
+}
+
+$sqlb = "SELECT * from likes where usuario = $id and post = $postid";
+    $res=mysqli_query($conn,$sqlb);
+    while($table=mysqli_fetch_array($res))
+    {
+        $curtval = $table["post"];
+    }
+
+if ($curtval == $postid) {
+  $curtidasform = "<form class='curtirf' action='php/curtir.php' method='post' target='_BLANK'>";
+} else {
+  $curtidasform = "<form class='curtir' action='php/curtir.php' method='post' target='_BLANK'>";
+}
+
+$msg = htmlspecialchars($msg);
+
+if ($tipo == 0) {
+
+  echo "<div id='postagem'>
+  <div id='postheader'>
+  <form action='perfil.php' class='icon' method='get' style='background-image: url(img/user_icons/$publishericon)'>
+  <input type='hidden' name='id' value='$publisherid'>
+  <input type='submit' value='' class='invico'>
+  </form>
+  <h1 class='pubname'> $publishername<b class='gray'>#$publisheruid</b></h1>
+  </div>
+  <div>
+  <p class='pubtxt'> $msg <br> Enviado na Comunidade: <b> <a href='perfil.php?id=$publisherid'> $coname </a> </b> </p>
+  </div>
+  <div class='acoes'>
+  <div></div>
+  $curtidasform
+  <input type='hidden' name='postid' value='$postid'>
+  <input type='submit' value='' class='invbtn'>
+  </form>
+  <div class='curtidas'> <h2> $curtidas </h2> </div>
+  <form class='pubview' action='post.php' method='get'>
+  <input type='hidden' name='publicacao' value='$postid'>
+  <input type='submit' value='Comentários' class='postvbtn'>
+  </form>
+  </div>
+  </div>";
+  
+  } else {
+  
+    echo "<div id='postagem'>
+    <div id='postheader'>
+    <form action='perfil.php' class='icon' method='get' style='background-image: url(img/user_icons/$publishericon)'>
+    <input type='hidden' name='id' value='$publisherid'>
+    <input type='submit' value='' class='invico'>
+    </form>
+    <h1 class='pubname'> $publishername<b class='gray'>#$publisheruid</b></h1>
+    </div>
+    <div>
+    <p class='pubtxt'> $msg </p>
+    <form class='foto' action='post.php' method='get'>
+    <input type='hidden' name='publicacao' value='$postid'>
+    <button type='submit' class='imabot'>
+    <img src='posts/$midia' class='imageviewer'>
+    </button>
+    </form>
+    </div>
+    <div class='acoes'>
+    <div></div>
+    $curtidasform
+    <input type='hidden' name='postid' value='$postid'>
+    <input type='submit' value='' class='invbtn'>
+    </form>
+    <div class='curtidas'> <h2> $curtidas </h2> </div>
+    <form class='pubview' action='post.php' method='get'>
+    <input type='hidden' name='publicacao' value='$postid'>
+    <input type='submit' value='Comentários' class='postvbtn'>
+    </form>
+    </div>
+    </div>";
+  
+  }
+  }
+      
+      ?>
+    </div>
     <p id="end"> Você chegou ao fim da navegação! </p>
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script type="text/javascript" src="js/ppostload.js"></script>
